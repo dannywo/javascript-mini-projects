0842ee9dc1fb441b604fe25274b06ff41a4afa86
dannywo <dannywo@gmail.com>
2021-07-24 02:53:08 -0400

    creates and instatiates variables. adds dummy list function.

--- fast-filtering/fast-filter.js
+++ fast-filtering/fast-filter.js
@@ -1,1 +1,27 @@
-console.lot("Loaded");
+console.clear();
+console.log("Loaded");
+
+const $ = s => document.querySelector(s);
+const $$ = s => document.querySelectorAll(s);
+
+(function(){
+    let list = [],
+        filteredList = [],
+        maxDisplayLimit = 10,
+        textInput = $('.text-filter'),
+        displayList = $('.list'),
+        countMessage = $('.count-message');
+
+    const generateDummyList = (itemCount) => {
+        if(!itemCount) return;
+        for (let i = 0; i < itemCount; i++) {
+            let item = {
+                name: Math.random().toString(36).substr(2, 10),
+                type: Math.random().toString(36).substr(2, 10),
+                category: Math.random().toString(36).substr(2, 10),
+            };
+
+            list.push(item);
+        }
+    }
+});
